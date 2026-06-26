016e4d368632cbb2ccf805767de5f90392fe8761
aabdullahsayed <abdullahalsayed087@gmail.com>
2026-06-26 22:09:45 +0600

    Add Binary Search

---
+++ Implementations/BinarySearch.java
@@ -0,0 +1,17 @@
+public class BinarySearch{
+
+
+public static void main(String[] args){
+
+int arr[] = {1,2,3,4,5,6,7};
+
+BinarySearchAlgo(arr);
+
+}
+
+public static void BinarySearchAlgo(int[] arr){
+
+
+}
+
+}
